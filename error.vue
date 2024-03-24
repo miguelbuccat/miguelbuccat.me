a11fa6808ddb3d0822dffd60d2aee42573cdec72
miguelbuccat <miguel.buccat12@gmail.com>
2024-03-25 01:10:25 +0800

    error page

---
+++ error.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <NuxtLayout>
+        <div class="flex flex-col min-h-[calc(100vh-60px)] justify-center items-center px-6">
+            <div class="flex flex-col gap-4 justify-center items-center text-center">
+                <h1 class="text-2xl font-semibold">Page not found</h1>
+                <p>Could not return the page you were looking for</p>
+            </div>
+        </div>
+    </NuxtLayout>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="min-h-screen flex flex-col bg-white">
     <div class="flex flex-row justify-between px-5 md:px-24 py-2 items-center h-[60px]">
-      <div class="flex flex-row gap-2 items-center">
+      <div class="flex flex-row gap-6 items-center">
         <RouterLink to="/" class="font-light text-xl">miguelbuccat.me</RouterLink>
+        <RouterLink to="/" class="hover:text-slate-800 hidden md:block">Home</RouterLink>
+        <RouterLink to="/about" class="hover:text-slate-800 hidden md:block">About me</RouterLink>
+        <RouterLink to="/projects" class="hover:text-slate-800 hidden md:block">Projects</RouterLink>
+        <RouterLink to="/blog" class="hover:text-slate-800 hidden md:block">Blog</RouterLink>
+      </div>
+      <div class="flex flex-row gap-6 items-center">
       </div>
     </div>
     <slot />
