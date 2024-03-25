fc06abb35e7749910526f56149519a855a0bcd36
miguelbuccat <miguel.buccat12@gmail.com>
2024-03-25 17:51:50 +0800

    about page

---
+++ pages/about.vue
@@ -0,0 +1,21 @@
+<script setup>
+const markdown = "### yes"
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center py-4 px-6">
+    <article class="prose shadow-md rounded-md py-4 px-6">
+      <h1>Hi! I'm Miguel Buccat</h1>
+      <p>
+        You can call me Miguel. I'm a 16 year old Filipino high school student.
+      </p>
+      <p>
+        I have been interested in web development since I was 10, when I started learning PHP. 
+        Since then, I've learned many languages and technologies, including 
+        PHP, Symfony, Laravel, NodeJS, ExpressJS, AdonisJS, VueJS, ReactJS and so forth. 
+        To view my full skillset, please do visit my <a href="https://github.com/miguelbuccat">Github</a> 
+      </p>
+    </article>
+  </div>
+</template>
